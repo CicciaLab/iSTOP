7046b2b2155c7ca3b3ce85242ea58d42ad39ea10
Eric Bryant <EricEdwardBryant@gmail.com>
2017-03-26 12:19:07 -0400

    Export plot_spliced_isoforms

--- R/plot-isoforms.R
+++ R/plot-isoforms.R
@@ -42,8 +42,36 @@ plot_isoforms <- function(gene, coords) {
   return(figure)
 }
 
+#' Plot spliced isoforms
+#'
+#' @param gene A single gene name
+#' @param coords A dataframe of [CDS coordinates][CDS] (will be filtered for `gene`)
+#' @param ticks_lower A dataframe of chromosome coordinates (see Details)
+#' @param ticks_upper A dataframe of chromosome coordinates (see Details)
+#'
+#' @details Chromosome coordinate dataframes must have two columns named `gene`, and `genome_coord`
+#' which correspond to gene name and genome coordinate values.
+#'
+#' @importFrom fuzzyjoin interval_left_join interval_inner_join
+#'
+#' @export
+#' @md
 
-plot_isoforms_spliced <- function(gene, coords, ticks_lower = NULL, ticks_upper = NULL) {
+plot_spliced_isoforms <- function(gene, coords, ticks_lower = NULL, ticks_upper = NULL, ticks_lower_color = 'black', ticks_upper_color = 'red') {
+  assertthat::assert_that(
+    assertthat::is.scalar(gene),
+    assertthat::has_name(coords, 'gene'),
+    assertthat::has_name(coords, 'tx'),
+    assertthat::has_name(coords, 'chr'),
+    assertthat::has_name(coords, 'strand'),
+    assertthat::has_name(coords, 'start'),
+    assertthat::has_name(coords, 'end'),
+    assertthat::has_name(ticks_lower, 'gene') | is.null(ticks_lower),
+    assertthat::has_name(ticks_lower, 'genome_coord') | is.null(ticks_lower),
+    assertthat::has_name(ticks_upper, 'gene') | is.null(ticks_upper),
+    assertthat::has_name(ticks_upper, 'genome_coord') | is.null(ticks_upper)
+  )
+
   g <- gene
   exons <- filter(coords, gene == g)
   txs   <- exons %>% group_by(tx) %>% summarise(size = sum(abs(start - end))) %>% arrange(size)
@@ -107,17 +135,23 @@ plot_isoforms_spliced <- function(gene, coords, ticks_lower = NULL, ticks_upper 
     )
 
   if (!is.null(ticks_lower)) {
-    df_ticks_lower <- data_frame(ticks_lower) %>%
-      interval_inner_join(exons_enumerated, by = c('ticks_lower' = 'start', 'ticks_lower' = 'end'))
+    df_ticks_lower <-
+      ticks_lower %>%
+      select(gene, genome_coord) %>%
+      filter(gene == g) %>%
+      interval_inner_join(exons_enumerated, by = c('genome_coord' = 'start', 'genome_coord' = 'end'))
     figure <- figure +
-      geom_rect(aes(xmin = ticks_lower, xmax = ticks_lower, ymin = tx_num - 0.3, ymax = tx_num - 0.05), color = 'black', data = df_ticks_lower)
+      geom_rect(aes(xmin = genome_coord, xmax = genome_coord, ymin = tx_num - 0.3, ymax = tx_num - 0.05), color = ticks_lower_color, data = df_ticks_lower)
   }
 
   if (!is.null(ticks_upper)) {
-    df_ticks_upper <- data_frame(ticks_upper) %>%
-      interval_inner_join(exons_enumerated, by = c('ticks_upper' = 'start', 'ticks_upper' = 'end'))
+    df_ticks_upper <-
+      ticks_upper %>%
+      select(gene, genome_coord) %>%
+      filter(gene == g) %>%
+      interval_inner_join(exons_enumerated, by = c('genome_coord' = 'start', 'genome_coord' = 'end'))
     figure <- figure +
-      geom_rect(aes(xmin = ticks_upper, xmax = ticks_upper, ymin = tx_num + 0.05, ymax = tx_num + 0.3), color = 'black', data = df_ticks_upper)
+      geom_rect(aes(xmin = genome_coord, xmax = genome_coord, ymin = tx_num + 0.05, ymax = tx_num + 0.3), color = ticks_upper_color, data = df_ticks_upper)
   }
 
   return(figure)
