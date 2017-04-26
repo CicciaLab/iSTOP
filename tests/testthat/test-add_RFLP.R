121b71266aa52a96b3215e760de2a2b39803646d
Eric Bryant <EricEdwardBryant@gmail.com>
2017-04-25 21:35:53 -0400

    Update tests

--- tests/testthat/test-add_RFLP.R
+++ tests/testthat/test-add_RFLP.R
@@ -13,16 +13,16 @@ test_that('RFLP matching considers both strands', {
   # But should not match if no edited base is present
   expect_equal(
     iSTOP:::identify_RFLP_enzymes('CCTC', enzymes),
-    ''
+    NA_character_
   )
   # If match on opposite strand then should match nothing and return an empty string
   expect_equal(
     iSTOP:::identify_RFLP_enzymes('GAGGCCTc', enzymes),
-    ''
+    NA_character_
   )
   # If match two overlapping sites then should return nothing
   expect_equal(
     iSTOP:::identify_RFLP_enzymes('CCTCCTc', enzymes),
-    ''
+    NA_character_
   )
 })
